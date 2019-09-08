e412607dcbf4bda27028c0fbf60140dc8a3518d1
Ebrar Islami <>
2019-09-09 01:20:53 +0200

    Login design improvements

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,12 @@ module.exports = {
   "plugins": [
     ["@babel/plugin-transform-flow-strip-types"],
     ["@babel/plugin-proposal-decorators", { "legacy": true }],
-    ["@babel/plugin-proposal-class-properties", { "loose": true }]
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+    ["module-resolver", {
+      "root": ["./src"],
+      "alias": {
+        "@components": "./src/@components",
+      }
+    }]
   ]
 }
--- src/screens/Auth/Login/LoginScreen.tsx
+++ src/screens/Auth/Login/LoginScreen.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TouchableOpacity, TextInput } from 'react-native';
-import { SafeAreaView, NavigationParams } from 'react-navigation';
+import React, { useRef } from 'react';
+import { StyleSheet, Text, View, StatusBar, TouchableOpacity, SafeAreaView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { NavigationParams } from 'react-navigation';
 import { inject, observer } from 'mobx-react';
 import styled from 'styled-components';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { ILoginStore } from '../../../stores/LoginStore';
 
 export interface Props {
@@ -14,127 +15,164 @@ interface State {
 
 }
 
-class LoginScreen extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.passwordInputRef = null;
-  }
+const LoginScreen: React.SFC<Props> = (props: Props) => {
 
-  passwordInputRef: TextInput | null;
+  const passwordInputRef = useRef(null);
+  const { error, loading, loadingFailed, email, password } = props.loginStore;
 
-  static navigationOptions = () => {
-    return {
-      header: null,
-    };
-  };
-
-  onLoginHandler = () => {
-    const { login } = this.props.loginStore;
+  const onLoginHandler = () => {
+    const { login } = props.loginStore;
     login();
   };
 
-  onSignupClickHandler = () => {
-    const { clear } = this.props.loginStore;
+  const onSignupClickHandler = () => {
+    const { clear } = props.loginStore;
     clear();
-    this.props.navigation.push('RegisterScreen')
+    props.navigation.push('RegisterScreen')
   }
 
-  onEmailChange = (value: string) => {
-    const { setEmail } = this.props.loginStore;
+  const onEmailChange = (value: string) => {
+    const { setEmail } = props.loginStore;
     setEmail(value);
   };
 
-  onPasswordChange = (value: string) => {
-    const { setPassword } = this.props.loginStore;
+  const onPasswordChange = (value: string) => {
+    const { setPassword } = props.loginStore;
     setPassword(value);
   };
 
-  onEndEmail = () => {
-    this.passwordInputRef && this.passwordInputRef.focus();
+  const onEndEmail = () => {
+    passwordInputRef && passwordInputRef.current && passwordInputRef.current.focus();
   }
 
-  onEndPassword = () => {
-    this.onLoginHandler();
+  const onEndPassword = () => {
+    onLoginHandler();
   }
 
-  render() {
-    const { email, password, loading, loadingFailed, error } = this.props.loginStore;
-    return (
-      <SafeArea>
-          <StatusBar backgroundColor="white" barStyle="dark-content" />
-          <View style={{flex: 1, marginTop: 40}}>
-            <TextInput
+  const onGooglePressed = () => {
+
+  }
+
+  const onFacebookPress = () => {
+
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1, margin: 16, justifyContent: 'center' }}>
+        <StatusBar backgroundColor="white" barStyle="dark-content" />
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <FormContainer>
+          <InputContainer>
+            <IconContainer>
+              <FontAwesome5 size={14} color='#8F9BB3' name={'user'} solid/>
+            </IconContainer>
+            <Input
               value={email}
-              onChangeText={(value) => this.onEmailChange(value)}
+              onChangeText={(value: string) => onEmailChange(value)}
               scrollEnabled={false}
-              onSubmitEditing={this.onEndEmail}
+              onSubmitEditing={onEndEmail}
               placeholderTextColor='#8F9BB3'
               autoCapitalize='none'
               placeholder='Email'
               returnKeyType='next'
               blurOnSubmit={false}
-              style={{
-                borderWidth: 1,
-                borderColor: '#8F9BB3',
-                backgroundColor: '#F7F9FC',
-                borderRadius: 5,
-                paddingLeft: 10,
-                paddingVertical: 15
-              }}
             />
-            <TextInput
-              ref={(input) => { this.passwordInputRef = input; }}
+          </InputContainer>
+          <InputContainer style={{marginTop: 25}}>
+            <IconContainer>
+              <FontAwesome5 size={14} color='#8F9BB3' name={'lock'} />
+            </IconContainer>
+            <Input
+              ref={passwordInputRef}
               value={password}
-              onChangeText={(value) => this.onPasswordChange(value)}
+              onChangeText={(value: string) => onPasswordChange(value)}
               scrollEnabled={false}
               placeholderTextColor='#8F9BB3'
               autoCapitalize='none'
               returnKeyType='go'
               placeholder='Password'
-              onSubmitEditing={this.onEndPassword}
+              onSubmitEditing={onEndPassword}
               blurOnSubmit={true}
               secureTextEntry
-              style={{
-                borderWidth: 1,
-                borderColor: '#8F9BB3',
-                backgroundColor: '#F7F9FC',
-                borderRadius: 5,
-                paddingLeft: 10,
-                marginTop: 25,
-                paddingVertical: 15
-              }}
             />
-            {
-              loadingFailed && <Text style={{color: 'red', fontWeight: 'bold', marginTop: 10, textAlign: 'center'}}>{error}</Text>
-            }
+          </InputContainer>
+          <Text style={{color: '#8F9BB3', fontWeight: 'bold', marginTop: 10, textAlign: 'right'}}>Forgot password?</Text>
+          {
+            <Text style={{ opacity: loadingFailed ? 1 : 0, color: 'red', fontWeight: 'bold', marginTop: 10, textAlign: 'center'}}>{error}</Text>
+          }
+          <SignButton
+            disabled={loading}
+            onPress={onLoginHandler}
+          >
+            <Text style={{color: 'white', fontWeight: 'bold'}}>{loading ? 'LOADING...' : 'SIGN IN'}</Text>
+          </SignButton>
+          <Text style={{color: '#8F9BB3', fontWeight: 'bold', marginTop: 20, textAlign: 'center'}}>Or</Text>
+          <View style={{marginTop: 20, alignItems: 'center', flexDirection: 'row', justifyContent: 'center',}}>
+            <TouchableWithoutFeedback onPress={onGooglePressed}>
+              <FontAwesome5 style={{marginRight: 15}} size={22} color='#8F9BB3' name={'google'} />
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={onFacebookPress}>
+              <FontAwesome5 size={22} color='#8F9BB3' name={'facebook'} />
+            </TouchableWithoutFeedback>
           </View>
-          <View style={{justifyContent: 'center', marginBottom: 8}}>
-            <TouchableOpacity
-              onPress={this.onLoginHandler}
-              style={{
-                backgroundColor: '#F7F9FC',
-                borderRadius: 5,
-                width: '100%',
-                paddingVertical: 20,
-                alignItems: 'center'
-              }}
-            >
-              <Text style={{color: '#C6CFE0', fontWeight: 'bold'}}>{loading ? 'LOADING...' : 'SIGN IN'}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={this.onSignupClickHandler}>
-              <Text style={{color: '#C6CFE0', fontWeight: 'bold', marginTop: 10, textAlign: 'center'}}>Sign Up</Text>
-            </TouchableOpacity>
-          </View>
-      </SafeArea>
-    );
-  }
-}
+        </FormContainer>
+        </TouchableWithoutFeedback>
+        <View style={{justifyContent: 'center', marginBottom: 8}}>
+          <TouchableOpacity onPress={onSignupClickHandler}>
+            <Text style={{color: '#8F9BB3', fontWeight: 'bold', marginTop: 10, textAlign: 'center'}}>Don't have account? Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+    </SafeAreaView>
+  );
+};
 
-const SafeArea = styled.SafeAreaView`
+LoginScreen.navigationOptions = () => {
+  return {
+    header: null
+  }
+};
+
+const FormContainer = styled.View`
   flex: 1;
-  margin: 16px;
-  justify-content: center;
+  margin-top: 170px;
 `;
 
+const InputContainer = styled.View`
+  position: relative;
+`;
+
+const IconContainer = styled.View`
+  position: absolute;
+  z-index: 1;
+  top: 20px;
+  left: 15px;
+`;
+
+const Input = styled.TextInput`
+  border-width: 1;
+  border-color: transparent;
+  background-color: white;
+  border-radius: 50px;
+  padding-left: 40px;
+  padding-top: 18px;
+  padding-bottom: 18px;
+  shadow-opacity: 0.75;
+  shadow-radius: 5;
+  shadow-color: rgba(0, 0, 0, .3);
+  shadow-offset: 0px 2px;
+`;
+
+const SignButton = styled.TouchableOpacity`
+  background-color: #62B4AD;
+  border-radius: 50px;
+  width: 100%;
+  padding: 18px 0px;
+  align-items: center;
+  shadow-opacity: 0.85;
+  shadow-radius: 4;
+  shadow-color: rgba(0, 0, 0, .3);
+  shadow-offset: 0px 4px;
+  margin-top: 45px;
+`;
 
 export default inject("loginStore")(observer(LoginScreen));
